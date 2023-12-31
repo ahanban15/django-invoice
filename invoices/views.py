8a006558e03c2b4ef034a69b36173865d64b2ae4
Ahan Bandyopadhyay <ahanbandyopadhyay@gmail.com>
2023-12-31 13:33:10 +0530

    final commit

---
+++ invoices/views.py
@@ -0,0 +1,12 @@
+# invoices/views.py
+from rest_framework import generics
+from .models import Invoice, InvoiceDetail
+from .serializers import InvoiceSerializer, InvoiceDetailSerializer
+
+class InvoiceListCreateView(generics.ListCreateAPIView):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
+
+class InvoiceRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Invoice.objects.all()
+    serializer_class = InvoiceSerializer
